13eb86688f13a782e408fd17c5413e80b6fe49a9
copyniinja <killfyz0.o@gmail.com>
2025-09-25 18:34:50 +0600

    const

---
+++ constant/main.go
@@ -0,0 +1,16 @@
+package main
+
+//const outside function
+const PI = 3.14
+
+func main() {
+    //const with type
+	const ISADMIN bool = true
+    
+	//const type inferred
+	const name = "Faiyaz"
+
+	//print
+	print(PI, ISADMIN, name)
+
+}
