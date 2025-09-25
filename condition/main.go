292de73ee0f9b32294d9e99f503a3be4391c86aa
copyniinja <killfyz0.o@gmail.com>
2025-09-25 22:47:13 +0600

    multi-case-switch

--- condition/main.go
+++ condition/main.go
@@ -36,8 +36,22 @@ func main() {
     fmt.Println("Saturday")
   case 7:
     fmt.Println("Sunday")
+  default:
+    fmt.Println("Not a weekday")
   }
 
+  //multi case
+  anotherDay := 5
 
+   switch anotherDay {
+   case 1,3,5:
+    fmt.Println("Odd weekday")
+   case 2,4:
+     fmt.Println("Even weekday")
+   case 6,7:
+    fmt.Println("Weekend")
+  default:
+    fmt.Println("Invalid day of day number")
+  }
 
 }
