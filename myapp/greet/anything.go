b6d1df69f067d851e2a416db859697d11a06e2fa
copyniinja <killfyz0.o@gmail.com>
2025-09-25 17:36:46 +0600

    variables

--- myapp/greet/anything.go
+++ myapp/greet/anything.go
@@ -5,11 +5,12 @@ import (
 )
 
 
-func SayHello(name string){
+
+func SayHello(name string)  {
      ping()
 	fmt.Println("Hello,",name)
 
-}
+  }
 
 func ping(){
 	fmt.Println("DING DONG, but its private!(first word small=>p )")
---
+++ variable/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+    // string
+    var name string
+    name = "Faiyaz Ahmed"
+
+    firstname := "Faiyaz"
+    var lastname = "Ahmed"
+
+    fmt.Println(name, firstname, lastname)
+
+    // number
+    var num = 1
+    fmt.Println("Number:", num)
+
+    var number int = 34
+    fmt.Println("Number:", number)
+
+    num1 := 15
+    num1 = 30
+    fmt.Println("Number:", num1)
+
+    // bool
+    isLogin := false
+    fmt.Println("isLogin:", isLogin)
+
+    var isOk bool
+    isOk = true
+    fmt.Println("isOk:", isOk)
+}
