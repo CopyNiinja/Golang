f7c70562fdaabe021fc6f578abf921494c4faa52
copyniinja <killfyz0.o@gmail.com>
2025-09-25 11:35:04 +0600

    module

---
+++ myapp/greet/anything.go
@@ -0,0 +1,16 @@
+package greet
+
+import (
+	"fmt"
+)
+
+
+func SayHello(name string){
+     ping()
+	fmt.Println("Hello,",name)
+
+}
+
+func ping(){
+	fmt.Println("DING DONG, but its private!(first word small=>p )")
+}
---
+++ myapp/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"myapp/greet"
+)
+
+
+func main(){
+    fmt.Println("HEY")
+	greet.SayHello("Alice")
+	
+}
