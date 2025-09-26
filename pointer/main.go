e46a7abbdc5450be395eb5bac69069185b327a68
copyniinja <killfyz0.o@gmail.com>
2025-09-26 19:51:14 +0600

    Pointer

---
+++ pointer/main.go
@@ -0,0 +1,48 @@
+package main
+
+//LIKE C,C++ NO POINTER ARITHMETIC IN GO :(
+import "fmt"
+func main(){
+
+	var x int = 10;
+	fmt.Println("x->",x); //10
+	fmt.Println("address of x->",&x); //0xc00008c060
+	fmt.Println("value of the address of x->",*(&x));//10
+
+   //initialize a pointer
+	var ptr *int;
+	ptr = &x;
+
+	fmt.Println("pointer holding address ptr->",ptr);//0xc00008c060
+	fmt.Println(" the value is located in address ptr->",*ptr); //10
+
+	//pointer can be used to pass the reference
+
+	a:=23;
+
+	change:=func(numPointer *int){
+		*numPointer= 500;
+	}
+	fmt.Println("a:",a);//23
+	change(&a);
+	fmt.Println("a:",a);//500
+
+	//If you pass a big struct to a function, Go copies the whole thing.
+    //Using a pointer avoids that cost.a
+
+
+
+type Person struct {
+    Name string
+    Age  int
+    Data [1000000]int // huge array so better just pass the address !!!
+}
+
+//   func updateAge(p *Person, newAge int) {
+//     p.Age = newAge
+// }
+
+	
+
+
+}
