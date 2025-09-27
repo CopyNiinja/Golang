8dd1ac14b1802c771aacd124e3770d269444b941
copyniinja <killfyz0.o@gmail.com>
2025-09-27 14:52:37 +0600

    struct custom constructor

--- struct/main.go
+++ struct/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 //Outside main {First letter capital will make it available in different packages}
 type User struct {
 	name string;
 	age int;
+}
+
+type Admin struct {
+	firstName string
+	lastName string
+	 bio string
+	 age int
+	 createdAt time.Time 
+}
+
+func newAdmin(fname,lname,bio string,age int)Admin{
+ return Admin{firstName: fname,lastName: lname,bio: bio,age: age,createdAt: time.Now()}
 }
 
 func main(){
@@ -28,4 +43,10 @@ func main(){
 
 	fmt.Println(parson1)
 	fmt.Println(user1)
+
+	//
+	var admin1 = Admin{ firstName: "Faiyaz",lastName:"Ahmed",bio: "dsd",age: 26,createdAt: time.Now()}
+   admin2 := newAdmin("Mushfiqur","Rahim","hehehe",43);
+	fmt.Println(admin1)
+	fmt.Println(admin2)
 }
