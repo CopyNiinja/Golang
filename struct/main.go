b68ce70568e8912e75e6b6ec3a03914b5d3f4e97
copyniinja <killfyz0.o@gmail.com>
2025-09-28 11:10:58 +0600

    struct embedding

--- struct/main.go
+++ struct/main.go
@@ -8,7 +8,7 @@ import (
 
 //Outside main {First letter capital will make it available in different packages}
 type User struct {
-	name string;
+	name string; //first letter should be capital to make it available in difference packages
 	age int;
 }
 
@@ -25,6 +25,26 @@ func newAdmin(fname,lname,bio string,age int)(*Admin,error){
      return nil,errors.New("Firstname and lastname are required!!")
 	}
  return &Admin{firstName: fname,lastName: lname,bio: bio,age: age,createdAt: time.Now()},nil
+}
+
+//struct embedding
+
+type Human struct {
+	name string
+	age int 
+	gen string
+}
+func (h Human) humanGen (){
+ fmt.Println(h.gen)
+}
+
+type Person2 struct{
+	Human
+	gen string
+
+}
+func (p Person2) PersonGen(){
+   fmt.Println(p.gen)
 }
 
 func main(){
@@ -53,4 +73,14 @@ func main(){
    var admin2  ,err= newAdmin("Mushfiqur","Rahim","hehehe",43);
 	fmt.Println(admin1)
 	fmt.Println(*admin2,err);
+
+	//struct embedding
+	p1 := Person2{Human{name: "bongchong",age:44,gen:"Homo Sapiens"} , "Upozati"}  //way 1
+	// p1 := Person2{Human:Human{name: "bongchong",age:44,gen:"Homo Sapiens"} , gen:"Upozati"}  //way 2
+	
+	fmt.Println(p1);//"{{bongchong 44 Homo Sapiens} Upozati}
+	fmt.Println(p1.Human);//"{bongchong 44 Homo Sapiens}
+	p1.humanGen()//homosapiens
+	p1.PersonGen()//Upozati
+	p1.Human.humanGen()//Homosapiens
 }
