19f5f9cb85907b8686134f0a2fe146c01639ce73
copyniinja <killfyz0.o@gmail.com>
2025-09-29 17:25:55 +0600

    interface complete

--- interface/moreInterface.go
+++ interface/moreInterface.go
@@ -6,7 +6,8 @@ import (
 )
 
 func ToString(val interface{})interface{} {
-
+	
+	
 	switch v:= val.(type){ // concrete type
 	case string:
 		return v
@@ -18,6 +19,17 @@ func ToString(val interface{})interface{} {
 		return fmt.Sprintf("%v", v) 	
 
 	}
+	//another way
+
+	value,ok :=val.(int); //if the value is int type then ok will be true,value will be the number argument
+	if(ok) {//means the value is int type
+		value+=1; //value type int not interface{} anymore
+        
+		} else{
+			//means the value is not int
+			//value will be 0 (default value)
+		}
+		return val
 
 }
 //Any Type (like typescript)
